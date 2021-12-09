f00c1d56171cea6b7a62c0a4bc4c5405fcc33aa8
Sesana Federico <sesana.federico@osdbsesto>
2021-12-09 11:34:20 +0100

    json stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var apiServer = express();
+var fs = require("fs");
 
 var port = 3000;
 var host = 'localhost';
@@ -24,4 +25,35 @@ apiServer.get("/somma", (request, response)=>{
     var a = parseInt(request.query.a);
     var b = parseInt(request.query.b);
     response.send('La somma è: ' + (a + b));
+});
+
+apiServer.get("/student", (request, response)=>{
+    console.log("student id: " + request.query.id);
+    fs.readFile("studenti.json", (err, data)=>{
+        if(err) console.log(err);
+        else {
+            var students = JSON.parse(data);
+            students.forEach(element => {
+                if (element.id == request.query.id) {
+                    console.log(element);
+                    response.send(element.id);
+                    //students.find(x => x.id === request.query.id)       <----Figo!!!
+                }
+            });
+        } 
+    })
+});
+
+apiServer.get("/newStudent", (request, response)=>{
+    var other = {"id" : request.query.id, "name" : request.query.name, "surname" : request.query.surname};
+    fs.readFile("studenti.json", (err, data) => {
+        if(err) console.log(err);
+        else {
+            var students = JSON.parse(data);
+            students.push(other);
+            fs.writeFile("studenti.json", JSON.stringify(students), (err)=>{
+                if(err) console.log(err);
+            });
+        }
+    });
 });
